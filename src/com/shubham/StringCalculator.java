c5176c9aa16d5f3c94e358fe9afdc04902a5447d
shubhamshukla7794 <shubhamshukla7794@gmail.com>
2021-04-08 13:54:12 +0530

    Code for Empty String

---
+++ src/com/shubham/StringCalculator.java
@@ -0,0 +1,12 @@
+package com.shubham;
+
+public class StringCalculator {
+	
+	public static int add(String numbers) {
+		if (numbers.isEmpty()) {
+			return 0;
+		}
+		return -1;
+	}
+	
+}
---
+++ src/com/shubham/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.shubham;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringCalculatorTest {
+
+	@Test
+	void testEmptyStr() {
+		assertEquals(0, StringCalculator.add(""));
+	}
+
+}
